a56fd5611e28e45c90ff5547585915b01a415fdb
ronaldchinzela <i201820124@cibertec.edu.pe>
2020-11-01 17:23:59 -0500

    Mi primer commit

---
+++ src/main/java/com/bcp/RestEjemploConBdApplication.java
@@ -0,0 +1,13 @@
+package com.bcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEjemploConBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEjemploConBdApplication.class, args);
+	}
+
+}
